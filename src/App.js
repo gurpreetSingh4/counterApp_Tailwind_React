988c6be5ac5b846e3c3fc787c6a921e9be756be1
gurpreetSingh4 <gurpreetyarasingh@gmail.com>
2024-04-12 18:52:31 +0530

    some changes for optimization

--- src/App.js
+++ src/App.js
@@ -18,17 +18,17 @@ function App() {
 
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
-      <div className="text-[#0398d4] font-medium text-2x ">Increment && Decrement</div>
+      <div className="text-[#0398d4] font-medium text-2xl ">Increment && Decrement</div>
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344115]">
         <button onClick={decHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
           -
         </button>
-        <div className="font-bold gap-12 text-5xl border-[#bfbfbf] text-5xl">
+        <div className="font-bold gap-12  text-5xl">
           {count}
         </div>
-        <buttom onClick={incHandler}className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+        <button onClick={incHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
           +
-        </buttom>
+        </button>
       </div>
       <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
         Reset
